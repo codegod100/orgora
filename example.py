c22b0f78ca36e34b727ba04739ad26e6d8358b05
𝕯𝖊𝖒𝖔𝖓 <78769380+codegod100@users.noreply.github.com>
2024-09-19 17:02:57 -0700

    release binary way smaller

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import extism
 
 input = open("fixtures/sample1.org", "r").read()
 expected = open("fixtures/sample1.html","r").read()
-manifest = {"wasm": [{"path": "target/wasm32-unknown-unknown/debug/orgora.wasm"}]}
+manifest = {"wasm": [{"path": "target/wasm32-unknown-unknown/release/orgora.wasm"}]}
 with extism.Plugin(manifest) as plugin:
     o = plugin.call(
         "parse_with_wasm",
